5da7fdc981fc6d8f96a77da6127bd1a2c7796c54
ShoyebOP <starshoyeb@gmail.com>
2026-02-16 19:07:14 +0600

    feat(nvim): Switch to colorizer and minty for better UX

--- nvim/.config/nvim/lua/plugins/color-picker.lua
+++ nvim/.config/nvim/lua/plugins/color-picker.lua
@@ -1,21 +1,50 @@
 return {
+  -- High-performance color highlighter
   {
-    "uga-rosa/ccc.nvim",
-    cmd = { "CccPick", "CccConvert", "CccHighlighterEnable", "CccHighlighterDisable", "CccHighlighterToggle" },
-    keys = {
-      { "<leader>cp", "<cmd>CccPick<cr>", desc = "Color Picker" },
-    },
+    "norcalli/nvim-colorizer.lua",
+    event = "VeryLazy",
     opts = {
-      highlighter = {
-        auto_enable = true,
-        lsp = true,
+      filetypes = { "*" },
+      user_default_options = {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = true, -- "Name" codes like Blue or blue
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        mode = "background", -- Set the display mode.
       },
     },
     config = function(_, opts)
-      require("ccc").setup(opts)
-      if opts.highlighter and opts.highlighter.auto_enable then
-        vim.cmd("CccHighlighterEnable")
-      end
+      require("colorizer").setup(opts.filetypes, opts.user_default_options)
     end,
+  },
+
+  -- UI Library required for Minty
+  { "NvChad/volt", lazy = true },
+
+  -- Visual Color Picker
+  {
+    "NvChad/minty",
+    cmd = { "Shades", "Huefi" },
+    keys = {
+      {
+        "<leader>cp",
+        function()
+          require("minty.huefi").open()
+        end,
+        desc = "Color Picker (Huefi)",
+      },
+      {
+        "<leader>cS",
+        function()
+          require("minty.shades").open()
+        end,
+        desc = "Color Shades",
+      },
+    },
   },
 }
