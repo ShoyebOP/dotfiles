463c7481b8e4b6f3ed2222339305d7a5de0fd141
ShoyebOP <starshoyeb@gmail.com>
2026-02-16 18:52:26 +0600

    feat(nvim): Prevent blink.cmp trigger after { in CSS files

--- nvim/.config/nvim/lua/plugins/css-completion.lua
+++ nvim/.config/nvim/lua/plugins/css-completion.lua
@@ -1,38 +1,20 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    ft = { "css", "scss", "less", "postcss" },
+    "saghen/blink.cmp",
     opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.completion = opts.completion or {}
-
-      -- Keep normal auto trigger
-      opts.completion.autocomplete = {
-        cmp.TriggerEvent.TextChanged,
-      }
-
-      -- Filter out '{' from trigger characters
-      local original_enabled = opts.enabled
       opts.enabled = function()
-        local context = require("cmp.config.context")
-        local col = vim.fn.col(".")
-        local line = vim.fn.getline(".")
-        local char_before = line:sub(col - 1, col - 1)
-
-        if char_before == "{" then
-          return false
+        local ft = vim.bo.filetype
+        if ft == "css" or ft == "scss" or ft == "less" or ft == "postcss" then
+          local col = vim.fn.col(".")
+          local line = vim.fn.getline(".")
+          local char_before = line:sub(col - 1, col - 1)
+          if char_before == "{" then
+            return false
+          end
         end
-
-        if type(original_enabled) == "function" then
-          return original_enabled()
-        elseif type(original_enabled) == "boolean" then
-          return original_enabled
-        end
-
-        return not context.in_treesitter_capture("comment")
-          and not context.in_syntax_group("Comment")
+        return not vim.tbl_contains({ "lua", "markdown" }, vim.bo.filetype)
+          or vim.bo.buftype ~= "prompt"
       end
-
       return opts
     end,
   },
